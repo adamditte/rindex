2ff67b52c01e250e5b41fc4ffe12141763931ef1
Adam Dittebrand <Adam Dittebrand>
2016-11-01 10:13:44 -0400

    first commit

---
+++ rindex.rb
@@ -0,0 +1,15 @@
+"write own rindex function without using reverse method or index method"
+
+"write a function that returns the last instance of the variable"
+
+array = ["a", "b", "b", "b", "b", "c"]
+
+# array1 = [array[5], array[4], array[3], array[2], array[1], array[0]]
+
+# array1.count("b")
+
+# array1.count
+
+def backwards(array)
+    [array[5], array[4], array[3], array[2], array[1], array[0]]
+end
